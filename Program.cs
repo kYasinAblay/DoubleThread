358a635a5ca85350522a01f0001a2629737648ec
Kamil Yasin ABLAY <KamilyasinAblay@hotmail.com>
2022-10-13 17:33:40 +0300

    oky

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ThreadDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sp = new Stopwatch();
+            sp.Start();
+            var thread = new Thread(() => Increment());
+
+            thread.Start();
+
+            WriteLine("\n");
+            var thread2 = new Thread(() => Decrement());
+            thread2.Start();
+
+            thread.Join();
+            sp.Stop();
+
+            WriteLine("Geçen süre: " + sp.Elapsed.Seconds);
+            ReadKey();
+        }
+
+        static void Increment()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Thread.Sleep(500);
+                WriteLine("Number: " + i.ToString());
+            }
+        }
+        static void Decrement()
+        {
+            for (int j = 10; j >= 0; j--)
+            {
+                Thread.Sleep(500);
+                WriteLine("Number: " + j.ToString());
+            }
+        }
+    }
+}
